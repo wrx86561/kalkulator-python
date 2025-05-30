39b72036b0076a3c5b5cddbc47ffb72aca5cf6c3
Dawid Gbur <wrx86561@student.wroclaw.merito.pl>
2025-05-30 10:52:41 +0200

    Inicjalny commit: kalkulator w Pythonie

---
+++ srckalkulator.py
@@ -0,0 +1,30 @@
+def dodaj(a, b):
+    return a + b
+
+def odejmij(a, b):
+    return a - b
+
+def mnoz(a, b):
+    return a * b
+
+def dziel(a, b):
+    if b == 0:
+        return "Błąd: Dzielenie przez zero"
+    return a / b
+
+if __name__ == "__main__":
+    print("Prosty kalkulator")
+    a = float(input("Podaj pierwszą liczbę: "))
+    b = float(input("Podaj drugą liczbę: "))
+    operacja = input("Wybierz operację (+, -, *, /): ")
+
+    if operacja == "+":
+        print("Wynik:", dodaj(a, b))
+    elif operacja == "-":
+        print("Wynik:", odejmij(a, b))
+    elif operacja == "*":
+        print("Wynik:", mnoz(a, b))
+    elif operacja == "/":
+        print("Wynik:", dziel(a, b))
+    else:
+        print("Nieznana operacja.")
